f02173978887831c2ac4adab6f2e9adfc449d2d4
saif <saifrehman9796@gmail.com>
2022-07-29 19:13:50 +0500

    add Icon in table search input

--- src/lib/pixel-table/pixel-table.tsx
+++ src/lib/pixel-table/pixel-table.tsx
@@ -1,80 +1,80 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
-import DataTable, { createTheme } from "react-data-table-component";
+import DataTable, { createTheme } from 'react-data-table-component'
 interface PixelTableProps {
-  columns: any;
-  data: any;
-  progressPending?: boolean;
-  isSearchable?: boolean;
-  paginationPerPage?: number;
+  columns: any
+  data: any
+  progressPending?: boolean
+  isSearchable?: boolean
+  paginationPerPage?: number
 }
-import "./table.scss";
-import { PixelFactoryContext } from "../pixel-factory/pixel-factory";
-import PixelInput from "../pixel-input/pixel-input";
-
+import './table.scss'
+import { PixelFactoryContext } from '../pixel-factory/pixel-factory'
+import PixelInput from '../pixel-input/pixel-input'
 
 createTheme(
-  "solarized",
+  'solarized',
   {
     text: {
-      primary: "#000000",
-      secondary: "#000000",
+      primary: '#000000',
+      secondary: '#000000'
     },
     background: {
-      default: "#ffffff",
+      default: '#ffffff'
     },
     context: {
-      background: "#cb4b16",
-      text: "#FFFFFF",
+      background: '#cb4b16',
+      text: '#FFFFFF'
     },
     divider: {
-      default: "#cecece",
+      default: '#cecece'
     },
     action: {
-      button: "rgba(0,0,0,.54)",
-      hover: "rgba(0,0,0,.08)",
-      disabled: "rgba(0,0,0,.12)",
-    },
+      button: 'rgba(0,0,0,.54)',
+      hover: 'rgba(0,0,0,.08)',
+      disabled: 'rgba(0,0,0,.12)'
+    }
   },
-  "light"
-);
+  'light'
+)
 
 const StyledBootstrapTable = styled(DataTable)`
-header {
-  display:none !important;
-}`;
+  header {
+    display: none !important;
+  }
+`
 const StyledLoaderContainer = styled.div`
   width: 100%;
   height: 100%;
   min-height: 500px;
-`;
+`
 const SearchPixelInput = styled.div`
   width: 30%;
-`;
+`
 const CustomLoader = () => {
-  const ref = React.useRef<HTMLDivElement>(null);
-  const { showLoader, hideLoader } = React.useContext(PixelFactoryContext);
+  const ref = React.useRef<HTMLDivElement>(null)
+  const { showLoader, hideLoader } = React.useContext(PixelFactoryContext)
 
   React.useEffect(() => {
     if (ref.current) {
-      showLoader(ref);
+      showLoader(ref)
     }
     return () => {
-      hideLoader(ref);
-    };
-  }, []);
+      hideLoader(ref)
+    }
+  }, [])
   return (
     <React.Fragment>
-      <StyledLoaderContainer className="pixeltable" ref={ref} />
+      <StyledLoaderContainer className='pixeltable' ref={ref} />
     </React.Fragment>
-  );
-};
+  )
+}
 const customFilter = (val, data) => {
   return data.filter((e) =>
     Object.values(e).some((v) => String(v).includes(val))
-  );
-};
+  )
+}
 
 export const PixelTable = React.forwardRef<HTMLTableElement, PixelTableProps>(
   (
@@ -88,40 +88,41 @@ export const PixelTable = React.forwardRef<HTMLTableElement, PixelTableProps>(
     },
     ref
   ) => {
-    const [filterText, setFilterText] = React.useState("");
+    const [filterText, setFilterText] = React.useState('')
     const [resetPaginationToggle, setResetPaginationToggle] =
-      React.useState(false);
+      React.useState(false)
 
     const subHeaderComponentMemo = React.useMemo(() => {
       const handleClear = () => {
         if (filterText) {
-          setResetPaginationToggle(!resetPaginationToggle);
-          setFilterText("");
+          setResetPaginationToggle(!resetPaginationToggle)
+          setFilterText('')
         }
-      };
+      }
 
       return (
         <SearchPixelInput>
           <PixelInput
-            placeholder="Search"
-            name="search"
+            showSearchIcon={true}
+            placeholder='Search'
+            name='search'
             onChange={(e) => setFilterText(e.target.value)}
             value={filterText}
           />
         </SearchPixelInput>
-      );
-    }, [filterText, resetPaginationToggle]);
+      )
+    }, [filterText, resetPaginationToggle])
 
     return (
       <StyledBootstrapTable
-        keyField="id"
-        classes="pixeltable"
-        headerClasses="tableheader"
+        keyField='id'
+        classes='pixeltable'
+        headerClasses='tableheader'
         bordered={false}
         columns={columns}
         pagination={true}
         paginationPerPage={paginationPerPage}
-        theme="solarized"
+        theme='solarized'
         data={customFilter(filterText, data)}
         bootstrap4
         responsive
@@ -132,8 +133,8 @@ export const PixelTable = React.forwardRef<HTMLTableElement, PixelTableProps>(
         ref={ref}
         {...rest}
       />
-    );
+    )
   }
-);
+)
 
-export default PixelTable;
+export default PixelTable
