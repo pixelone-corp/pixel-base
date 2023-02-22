c98dd6e889306f62d646479a69823e82d9b5895d
Usama Jibran <usajibran@gmail.com>
2023-02-22 23:56:18 +0500

    exported PixelImage and PixelFlexBox in index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,8 @@ export * from './lib/pixel-dropzone/pixel-dropzone'
 export * from './lib/pixel-date/pixel-date'
 export * from './lib/pixel-input-tag/PixelInputTag'
 export * from './lib/pixel-text/pixel-text'
+export * from './lib/pixel-image/pixel-image'
+export * from './lib/pixel-flex-box/pixel-flex-box'
 import PixelFactoryContext, {
   PixelFactoryContext as PixelFactory
 } from './lib/pixel-factory/pixel-factory'
