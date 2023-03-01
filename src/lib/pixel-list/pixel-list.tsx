b51ecb757e27dcbe1b8760d9a099e8cee1d98ea3
Zeeshan Saqib <saqibzeeshan60@gmail.com>
2023-03-01 11:37:25 +0500

    PixelList and Listitem created

--- src/lib/pixel-list/pixel-list.tsx
+++ src/lib/pixel-list/pixel-list.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import styled  from 'styled-components';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react'
+import styled from 'styled-components'
 
-export interface PixelListProps{}
+export interface PixelListProps {
+  children?: React.ReactNode
+}
 
-export function PixelList(props: PixelListProps) {
-  return(
+const PixelList = (props: PixelListProps) => {
+  return (
     <React.Fragment>
-      <ListContainer>
-        <h1>Pixel List</h1>
-      </ListContainer>
+      <ListContainer {...props}>{props.children}</ListContainer>
     </React.Fragment>
   )
-};
+}
 
 const ListContainer = styled.div`
-  color: #9B02FD !important;
+  color: #9b02fd !important;
+  border: 1px solid Lightgray;
+  height: auto;
+  width: auto;
+  min-width: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 5px;
 `
 
-export default PixelList;
+export default PixelList
