62b6b7ff02cddb8e708618c9aefd44fb7ed66d9a
Muhammad Zeeshan Saqib <121866121+zeeshansaqib@users.noreply.github.com>
2025-02-03 18:11:05 +0500

    updates in dcinput

--- src/lib/dc-date-range-picker/dcDateRangePicker.stories.tsx
+++ src/lib/dc-date-range-picker/dcDateRangePicker.stories.tsx
@@ -1,31 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import DcDateRangePicker from './dcDateRangePicker'
 import { addDays } from 'date-fns'
+
 export default {
   title: 'DC DateRangePicker',
   component: DcDateRangePicker
 } as ComponentMeta<typeof DcDateRangePicker>
+
 const Template: ComponentStory<typeof DcDateRangePicker> = (args) => {
   const [state, setState] = useState<any>({
     start_date: new Date(),
-    end_date: addDays(new Date(), 7)
+    end_date: new Date()
   })
 
+  // Set default date range to last 7 days
+  useEffect(() => {
+    setState({
+      start_date: addDays(new Date(), -30),
+      end_date: new Date()
+    })
+  }, [])
+
   const onApply = () => {
-    alert('this is applied funtion')
+    alert('Date range applied!')
   }
+
   return (
     <React.Fragment>
       <DcDateRangePicker
         onChange={(e: any) => {
           setState({
-            ...state,
-            start_date: e.target.value?.range1?.startDate,
-            end_date: e?.range1?.endDate
+            start_date: e.range1?.startDate || state.start_date,
+            end_date: e.range1?.endDate || state.end_date
           })
         }}
-        ranges={state}
+        ranges={{
+          start_date: state.start_date,
+          end_date: state.end_date
+        }}
         month={2}
         onApply={onApply}
         position='right'
@@ -35,4 +48,5 @@ const Template: ComponentStory<typeof DcDateRangePicker> = (args) => {
     </React.Fragment>
   )
 }
+
 export const Simple = Template.bind({})
